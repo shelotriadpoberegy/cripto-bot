d4e85fb59022b803cb709527fb05b4074ab53e3f
shelotriadpoberegy <116545879+shelotriadpoberegy@users.noreply.github.com>
2023-02-03 17:00:52 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+
+TOKEN = '5895250425:AAFHVpUPjbNAW6AgnMoH241a_yZtkCzTJLA'
+
+keys = {
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+
+}
